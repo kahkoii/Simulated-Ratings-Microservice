9629eeb5cf1147b961f3a25357db6c26a0ed3fa7
Kah Ho <sweekahho@gmail.com>
2022-01-26 19:06:38 +0800

    chore: update deployment scripts to support up and down

--- deploy_scripts/windows.bat
+++ deploy_scripts/windows.bat
@@ -1,2 +1,14 @@
-docker run -d -p 8131:8131 -v sqlite-db:/app/db --name 13_api-server kahkoii/eti-asgt-api:latest
-docker run -d -p 8130:3000 --name 13_frontend-server kahkoii/eti-asgt-frontend:latest
+@echo off
+IF [%1] == [up] (
+    echo Starting eti-asgt-api and eti-asgt-frontend microservices...
+    docker pull kahkoii/eti-asgt-api:latest
+    docker pull kahkoii/eti-asgt-frontend:latest
+    docker run -p 8131:8131 -v sqlite-db:/app/db --name 13_api-server -d --rm kahkoii/eti-asgt-api:latest
+    docker run -p 8130:3000 --name 13_frontend-server -d --rm kahkoii/eti-asgt-frontend:latest
+) ELSE IF [%1] == [down] (
+    echo Stopping eti-asgt-api and eti-asgt-frontend microservices...
+    docker stop 13_api-server
+    docker stop 13_frontend-server
+) ELSE (
+    echo Usage: %0 up^|down
+)
